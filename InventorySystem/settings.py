3187393c14539ca52c89bcbb80c8c282f95fe8e8
aernest njuki <74731728+Aernestnjuki@users.noreply.github.com>
2024-10-01 23:50:48 +0300

    fourth commit

--- InventorySystem/settings.py
+++ InventorySystem/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_^u5)wgy)kt5*s!g=!3*f(l1sf5m%a(($l^bp+w#h17%ulf-w)'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-_^u5)wgy)kt5*s!g=!3*f(l1sf5m%a(($l^bp+w#h17%ulf-w)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1" # 1 == True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] # Change this before going to production
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('DJANGO_ALLOWED_HOSTS')]
 
 
 # Application definition
